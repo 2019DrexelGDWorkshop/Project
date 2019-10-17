9b69c36f734c94df7fcb5a96b12f2fe4fce6a647
xinxuan1587 <sdplguanli@gmail.com>
2019-10-16 20:04:56 -0400

    Combined character controller with the level design

---
+++ Assets/Codes/Scripts/TriggerAnim.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnim : MonoBehaviour
+{
+    private Animator _animator;
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        _animator.SetBool("Come", true);
+    }
+}
