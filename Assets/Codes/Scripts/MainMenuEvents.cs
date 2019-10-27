8fd949373574f0c73907715293226808066ccce1
DESKTOP-9LSN189\Nicholas Wegfahrt <ngw27@drexel.edu>
2019-10-27 16:21:37 -0400

    Complete Main Menu

---
+++ Assets/Codes/Scripts/MainMenuEvents.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuEvents : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayDemo()
+    {
+        print("Request to Play");
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
