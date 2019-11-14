51cee47ea47539004a85ac334651498a2764f08e
glangenhorst <glangenhorst@gmail.com>
2019-11-14 10:06:20 -0500

    EGS Build
    Level 1-3 added, music and temp SFX added

--- Assets/Codes/Scripts/DeadZone.cs
+++ Assets/Codes/Scripts/DeadZone.cs
@@ -6,11 +6,16 @@ using UnityEngine.SceneManagement;
 public class DeadZone : MonoBehaviour
 {
     public Transform startPos;
-
+    public AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Player")
         {
+            audioSource.Play();
             other.gameObject.transform.position = GameManager.Instance.lastCheckPoint.position;
         }
     } 
---
+++ Assets/sfxLiarScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sfxLiarScript : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] listJump;
+    public AudioClip[] listWhoosh;
+    private AudioClip sfxClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int index = Random.Range(0, listJump.Length);
+            sfxClip = listJump[index];
+            audioSource.clip = sfxClip;
+            audioSource.Play();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            int index = Random.Range(0, listWhoosh.Length);
+            sfxClip = listWhoosh[index];
+            audioSource.clip = sfxClip;
+            audioSource.Play();
+        }
+    }
+}
