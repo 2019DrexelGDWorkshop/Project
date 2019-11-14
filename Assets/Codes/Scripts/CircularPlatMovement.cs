3f7020d4d239d63083ce6e17c281c053180317d8
DESKTOP-9LSN189\Nicholas Wegfahrt <ngw27@drexel.edu>
2019-11-14 13:11:52 -0500

    moving plat smoothing

---
+++ Assets/Codes/Scripts/CircularPlatMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularPlatMovement : MonoBehaviour
+{
+    [SerializeField]
+    Transform center;
+
+    [SerializeField]
+    float radius = 2f;
+
+    [SerializeField]
+    float speed = 2f;
+
+    [SerializeField]
+    float angle = 360f;
+
+    float zPos;
+
+    float yPos;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        zPos = center.position.z + Mathf.Cos(angle) * radius;
+        yPos = center.position.y + Mathf.Sin(angle) * radius;
+        transform.position = new Vector3(transform.position.x, yPos, zPos);
+        angle = angle - Time.deltaTime * speed;
+
+        if ( angle <= 0f)
+        {
+            angle = 360f;
+        }
+    }
+}
---
+++ Assets/Codes/Scripts/RisingPlatform.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    float VerticalMax;
+
+    [SerializeField]
+    float VerticalMin;
+
+    [SerializeField]
+    float platSpeed;
+
+    [SerializeField]
+    bool movingUp;
+
+    Transform plat;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movingUp = true;
+        plat = this.GetComponent<Transform>();
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (plat.position.y >= VerticalMax)
+        {
+            movingUp = false;
+            print(plat.position.y);
+        }
+        else if (plat.position.y <= VerticalMin)
+        {
+            movingUp = true;
+            print(plat.position.y);
+        }
+
+        if (movingUp && rb.velocity.y <= platSpeed )
+        {
+            //plat.position = new Vector3(plat.position.x, VerticalMax, plat.position.z);
+            print("Added Up Force");
+            rb.AddForce(new Vector3(0, 2.0f, 0));
+        }
+        else if (!movingUp && rb.velocity.y >= -1*platSpeed )
+        {
+            //plat.position = new Vector3(plat.position.x, VerticalMin, plat.position.z);
+            print("Added Down Force");
+            rb.AddForce(new Vector3(0, -2.0f, 0));
+        }
+    }
+}
