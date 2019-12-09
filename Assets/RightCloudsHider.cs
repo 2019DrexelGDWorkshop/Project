009aa121b74552332d1be9989c185ee5e0a6dcfb
glangenhorst <glangenhorst@gmail.com>
2019-12-09 18:16:59 -0500

    CloudsHider Script added

---
+++ Assets/RightCloudsHider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightCloudsHider : MonoBehaviour
+{
+    public Renderer rend;
+
+
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        if (GameManager.gameManager.cameraState == 1)
+        {
+            rend.enabled = false;
+        }
+        else
+        {
+            rend.enabled = true;
+        }
+    }
+}
