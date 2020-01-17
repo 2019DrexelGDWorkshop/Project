bd931cfebd2756d34f5b23858016cf35144b3615
Chris McCole <chrismccole1@gmail.com>
2020-01-17 10:12:26 -0500

    Enemy folder amd base script.

---
+++ Assets/Scripts/Enemies/Enemy_base.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_base : MonoBehaviour
+{
+    #region Attributes
+
+    [Min(0), SerializeField]
+    protected ushort health;
+
+    #endregion
+
+    #region Getters Setters
+
+    public ushort GetHealth()
+    {
+        return health;
+    }
+
+    public void SetHealth(ushort _val)
+    {
+        health = _val;
+    }
+
+    public void SetHealth(int _val)
+    {
+        health = (ushort) _val;
+    }
+
+    #endregion
+
+    #region Virtual
+    protected virtual void Die()
+    {
+        Debug.Log(gameObject + "Died");
+        Destroy(this);
+    }
+    #endregion
+}
