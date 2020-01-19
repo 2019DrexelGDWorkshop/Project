1096c41aad6de1f2a08be7251de1755bbeb97d70
Chris McCole <chrismccole1@gmail.com>
2020-01-18 23:08:52 -0500

    Adding functionality to base enemy class.

--- Assets/Scripts/Enemies/Enemy_base.cs
+++ Assets/Scripts/Enemies/Enemy_base.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_base : MonoBehaviour
+public abstract class Enemy_base : MonoBehaviour
 {
     #region Attributes
 
     [Min(0), SerializeField]
-    protected ushort health;
+    protected ushort health = 1;
+
+    [Min(0), SerializeField]
+    protected ushort maxHealth = 1;
+
+    [SerializeField]
+    protected ushort minHealth = 0;
 
     #endregion
 
@@ -30,11 +36,69 @@ public class Enemy_base : MonoBehaviour
 
     #endregion
 
+    #region Monobehaviour
+
+    private void Awake()
+    {
+        CustomAwake();
+    }
+
+    #endregion
+
+    #region Protected
+
+    #region SubtractHealth
+    protected void SubtractHealth(ushort _val)
+    {
+        health -= _val;
+        if(health < minHealth)
+        {
+            Die();
+        }
+    }
+
+    protected void SubtractHealth(int _val)
+    {
+        health -= (ushort)_val;
+        if(health < minHealth)
+        {
+            Die();
+        }
+    }
+    #endregion
+
+    #region AddHealth
+    protected void AddHealth(ushort _val)
+    {
+        health += _val;
+        health = (ushort) Mathf.Min(maxHealth, health + _val);
+    }
+
+    protected void AddHealth(int _val)
+    {
+        health += (ushort)Mathf.Min(maxHealth, health + _val);
+    }
+    #endregion
+
+    #endregion
+
     #region Virtual
+    protected virtual void CustomAwake()
+    {
+
+    }
+
     protected virtual void Die()
     {
         Debug.Log(gameObject + "Died");
         Destroy(this);
     }
     #endregion
+
+    #region Abstract
+
+    protected abstract void OnTriggerEnter(Collider _col);
+
+    #endregion
+
 }
