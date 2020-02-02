c03a81837f3a6ec3fe5d00c579508a5787e82900
Jason <jwang11211@gmail.com>
2020-02-02 14:52:02 -0500

    added falling platform script and prefab

---
+++ Assets/Scripts/Level/FallingPlatform.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    [SerializeField, Tooltip("Seconds before platform falls")]
+    public float fallDelay = 2.0f;
+
+    [Tooltip("Seconds before platform respawns after falling")]
+    public float respawnDelay = 5.0f;
+
+    private Vector3 origPosition;
+
+    private void Start()
+    {
+        origPosition = gameObject.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine("FallAfterDelay");
+        }
+
+    }
+
+    IEnumerator FallAfterDelay()
+    {
+
+        yield return new WaitForSeconds(fallDelay);
+        GetComponent<Rigidbody>().isKinematic = false;
+        StartCoroutine("RespawnAfterDelay");
+
+
+
+    }
+
+    IEnumerator RespawnAfterDelay()
+    {
+        yield return new WaitForSeconds(respawnDelay);
+        gameObject.transform.position = origPosition;
+        GetComponent<Rigidbody>().isKinematic = true;
+
+    }
+}
