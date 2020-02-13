52f6ef059dac226f3289ea1bbfa5aeef0268c3ce
glangenhorst <glangenhorst@gmail.com>
2020-02-13 13:28:21 -0500

    Week 5 - Beta 2

---
+++ Assets/Scripts/DiaEdit/DialogueComplete.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueComplete : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public bool inRange = false;
+
+
+    public Text nameText;
+    public Text dialogueText;
+    public bool inChat = false;
+    public GameObject DiaUIobj;
+
+    //public Animator animator;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    void Update()
+    {
+        DiaUIobj.SetActive(inChat);
+    }
+    public void StartDialogue(Dialogue dialogue)
+    {
+        //animator.SetBool("IsOpen", true);
+        inChat = true;
+        Debug.Log("Starting conversation with " + dialogue.name);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        //Debug.Log(sentence);
+        //dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        //animator.SetBool("IsOpen", false);
+        inChat = false;
+        //Debug.Log("End of conversation.");
+    }
+}
---
+++ Assets/Scripts/DiaEdit/DialogueManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    public bool inChat = false;
+    public GameObject DiaUIobj;
+
+    //public Animator animator;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    void Update()
+    {
+        if (inChat)
+        {
+            if (!FindObjectOfType<NPCBehavior>().inRange)
+            {
+                EndDialogue();
+            }
+        }
+        //DiaUIobj.SetActive(inChat);
+    }
+    public void StartDialogue (Dialogue dialogue)
+    {
+        //animator.SetBool("IsOpen", true);
+        inChat = true;
+        DiaUIobj.SetActive(inChat);
+        Debug.Log("Starting conversation with " + dialogue.name);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        //Debug.Log(sentence);
+        //dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        //animator.SetBool("IsOpen", false);
+        inChat = false;
+        DiaUIobj.SetActive(inChat);
+        FindObjectOfType<NPCBehavior>().chatStarted = false;
+        //Debug.Log("End of conversation.");
+    }
+}
