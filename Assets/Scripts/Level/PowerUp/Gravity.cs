d956becafcebf120881b15e35ca954e89757f504
Li Guan <sdplguanli@gmail.com>
2020-02-22 23:18:06 -0500

    Add shadow on player and falling object system

---
+++ Assets/Scripts/Level/PowerUp/Gravity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public GameObject fallingObject;
+    public int delayTime=5;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            yield return new WaitForSeconds(delayTime);
+            fallingObject.GetComponent<Rigidbody>().useGravity = true;
+
+        }
+    }
+    
+
+}
