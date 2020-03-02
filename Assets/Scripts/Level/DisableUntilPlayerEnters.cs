73f81cacb9d3a8bc45d8f52e2c30a7a58487ba9e
Walt Gottlieb <whg29@drexel.edu>
2020-03-02 01:37:19 -0500

    Moving platofrm scale down fix and doesn't start moving until you get close to it

---
+++ Assets/Scripts/Level/DisableUntilPlayerEnters.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableUntilPlayerEnters : MonoBehaviour
+{
+    public GameObject movingPlatform;
+
+    void Start()
+    {
+        movingPlatform.GetComponent<PlatformMoving>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            movingPlatform.GetComponent<PlatformMoving>().enabled = true;
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
