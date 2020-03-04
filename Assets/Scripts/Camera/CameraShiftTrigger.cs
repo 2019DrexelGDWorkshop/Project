993794c9e2cb4f113937c17c18f5b61fb4f4f1c6
Chris McCole <chrismccole1@gmail.com>
2020-03-03 23:13:32 -0500

    Minor fixes to level 1. Camera pan, moving plat constraint. Crayons moved.

---
+++ Assets/Scripts/Camera/CameraShiftTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShiftTrigger : MonoBehaviour
+{
+    [SerializeField] private float shiftAmoutY = -.2f;
+    [SerializeField] private float shiftAmoutX = -.2f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject != LevelManager.Instance.player)
+            return;
+
+        CameraManager.Instance.ScreenShiftY(shiftAmoutY);
+        CameraManager.Instance.ScreenShiftX(shiftAmoutX);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject != LevelManager.Instance.player)
+            return;
+
+        CameraManager.Instance.ScreenShiftY();
+        CameraManager.Instance.ScreenShiftX();
+    }
+}
