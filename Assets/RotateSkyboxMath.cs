731a44ae707d08d7860419c5cb58f756d0ea11de
glangenhorst <glangenhorst@gmail.com>
2020-03-04 14:16:35 -0500

    Math + Gym Skyboxes Added

---
+++ Assets/RotateSkyboxMath.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkyboxMath : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", (Time.time * 1.5f));
+    }
+}
