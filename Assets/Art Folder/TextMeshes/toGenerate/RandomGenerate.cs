d1627432b781763d654332ee2da0f760b0856056
glangenhorst <glangenhorst@gmail.com>
2020-03-09 06:12:21 -0400

    Environment Work

---
+++ Assets/Art Folder/TextMeshes/toGenerate/RandomGenerate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGenerate : MonoBehaviour
+{
+    public Transform gennerGO;
+    public GameObject[] mathsSymbols;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instantiate(mathsSymbols[Random.Range(0, mathsSymbols.Length)], gennerGO.transform.position, gennerGO.transform.rotation);
+        //Destroy(this);
+    }
+}
---
+++ Assets/Scripts/CloudsScripts/RightLockerHider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightLockerHider : MonoBehaviour
+{
+    public GameObject[] selfLocker;
+    void Update()
+    {
+        if (CameraManager.Instance.cameraState == CameraState.SIDE_SCROLLER)
+        {
+            foreach (GameObject j in selfLocker)
+            {
+                j.SetActive(false);
+            }
+            
+        }
+        else
+        {
+            foreach (GameObject j in selfLocker)
+            {
+                j.SetActive(true);
+            }
+        }
+    }
+    //IEnumerator ExampleCoroutine()
+    //{
+        //yield return new WaitForSeconds(.5f);
+        //selfLocker.SetActive(true);
+    //}
+
+    //IEnumerator WaitThenOff()
+    //{
+    //    yield return new WaitForSeconds(.5f);
+    //    selfLocker.SetActive(false);
+    //}
+}
