f2e0283ac0f16ab6c811e20c1ffea351cf6f973f
glangenhorst <glangenhorst@gmail.com>
2020-03-12 13:43:51 -0400

    Lockers Fixed

---
+++ Assets/RightLockers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightLockers : MonoBehaviour
+{
+    public GameObject RightLockers1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CameraManager.Instance.cameraState == CameraState.SIDE_SCROLLER)
+        {
+            RightLockers1.SetActive(false);
+        }
+        else
+        {
+            RightLockers1.SetActive(true);
+        }
+    }
+}
