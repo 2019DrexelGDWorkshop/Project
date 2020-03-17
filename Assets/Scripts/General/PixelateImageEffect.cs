f317285ccbe40aff4cc1059dc8dedf65124080c1
glangenhorst <glangenhorst@gmail.com>
2020-03-17 00:24:37 -0400

    PixelateDeathEffect

--- Assets/Scripts/General/PixelateImageEffect.cs
+++ Assets/Scripts/General/PixelateImageEffect.cs
@@ -6,14 +6,34 @@ using UnityEngine;
 public class PixelateImageEffect : MonoBehaviour
 {
     public Material effectMaterial;
+    public int decInt = 200;
+    public bool goDown;
 
     void Start()
     {
-        
+        decInt = 200;
+        goDown = true;
     }
-    void Update()
+    void LateUpdate()
     {
-        
+        effectMaterial.SetFloat("_Columns", decInt);
+        effectMaterial.SetFloat("_Rows", decInt);
+        if (goDown)
+        {
+            decInt -= 3;
+        }
+        if (!goDown)
+        {
+            decInt += 10;
+        }
+        if (decInt <= 0)
+        {
+            goDown = false;
+        }
+        if (decInt >= 500)
+        {
+            //kill this script
+        }
     }
 
     void OnRenderImage(RenderTexture source, RenderTexture destination) {
